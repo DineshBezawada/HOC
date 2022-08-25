e2b3fc44e660bcfb39ffe69185564ea491ee29ad
dinesh <dinesh@gmail.com>
2022-08-25 15:51:44 +0530

    new

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import A from './Components/A';
+import B from './Components/B';
+
+function App() {
+  return (
+    <div className="App">
+    <A/>
+    <B/>
+    </div>
+  );
+}
+
+export default App;
